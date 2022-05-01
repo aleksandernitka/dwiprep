1ac0fd3290c8f273daad04c41a71a5c2609c0738
Aleksander Nitka <stxawn@nottingham.ac.uk>
2022-05-01 22:58:02 +0200

    Update denoise_data.py

--- denoise_data.py
+++ denoise_data.py
@@ -2,13 +2,14 @@ import pandas as pd
 import numpy as np
 from os import listdir
 from os.path import join, exists
+from dipy.io.image import load_nifti as load
+
 from skimage.metrics import peak_signal_noise_ratio as psnr
 from skimage.metrics import structural_similarity as ssim
 from skimage.metrics import mean_squared_error as mse
 from skimage.measure import euler_number as euler
 from skimage.measure import shannon_entropy as shannon
-import numpy as np
-from dipy.io.image import load_nifti as load
+from dipy.denoise.noise_estimate import estimate_sigma
 
 def snr(a, axis=None, ddof=0):
     """
@@ -36,46 +37,105 @@ def snr(a, axis=None, ddof=0):
     sd = a.std(axis=axis, ddof=ddof)
     return np.where(sd == 0, 0, m/sd)
 
+def calc_metrics(img):
+    
+    # Calculate single img metrics
+    m_sig = []
+    m_snr = []
+    m_eul = []
+    m_sha = []
+    # Calcualte comparison metrics
+    m_psnr = []
+    m_ssim = []
+    m_mser = []
+    
+    for v in range(0, img.shape[3]):
+        m_sig.append(estimate_sigma(img[:,:,:,v], N = 32)[0])
+        m_snr.append(snr(img[:,:,:,v]))
+        m_eul.append(euler(img[:,:,:,v]))
+        m_sha.append(shannon(img[:,:,:,v]))
+        
+        m_psnr.append(psnr(ibase[:,:,:,v], img[:,:,:,v]))
+        m_ssim.append(ssim(ibase[:,:,:,v], img[:,:,:,v]))
+        m_mser.append(mse(ibase[:,:,:,v], img[:,:,:,v]))
+        
+    return m_sig, m_snr, m_eul, m_sha, m_psnr, m_ssim, m_mser
+
 edir ='/mnt/nasips/aleksander/experiments/data/denoise_compare/'
+edir = '/Users/admin/x2goSwap/'
 
 if exists(edir):
     subs = [f for f in listdir(edir) if f.startswith('sub')]
+    subs = ['sub-17188']
     print(f'found {len(subs)} subject(s)')
 
     # for each subject
     for i, s in enumerate(subs):
         print(f'Processing {s}: {i} out of {len(subs)}')
+        
+        # Load base image and calculate 
+        ibase, __ = load(join(edir, s, f'{s}_AP.nii'))
+        
+        # calculate image metrics for base image
+        base_sig = []
+        base_snr = []
+        base_eul = []
+        base_sha = []
+        
+        for v in range(0, ibase.shape[3]):
+            base_sig.append(estimate_sigma(ibase[:,:,:,v], N=32)[0])
+            base_snr.append(snr(ibase[:,:,:,v]))
+            base_eul.append(euler(ibase[:,:,:,v]))
+            base_sha.append(shannon(ibase[:,:,:,v]))
+           
+        np.save(join(edir, s, f'{s}_base_sigma.npy'), base_sig)
+        np.save(join(edir, s, f'{s}_base_snr.npy'), base_snr)
+        np.save(join(edir, s, f'{s}_base_euler.npy'), base_eul)
+        np.save(join(edir, s, f'{s}_base_shannon.npy'), base_sha)
+            
+        # for the rest of images
+        # set filename suffixes and data name prefixes
+        fnames = ['_ap_gauss.nii.gz', '_ap_lpca.nii.gz', '_ap_mppca.nii.gz', \
+                  '_ap_nlm.nii.gz', '_ap_mrtrix.nii.gz', '_AP_denoised.nii.gz']
+        dnames = ['gauss', 'lpca', 'mppca', 'nlm', 'mrtrix', 'p2s']
+        
+        for j, f in enumerate(fnames):
+            
+            print(f'Processing file: {s}{f}')
+            
+            # Load image
+            img, __ = load(join(edir, s, f'{s}{f}'))
+            
+            # Calculate metrics
+            xsig, xsnr, xeuler, xshan, xpsnr, xssim, xmse = calc_metrics(img)
+            
+            # Save metrics to file
+            np.save(join(edir, s ,f'{s}_{dnames[j]}_sigma.npy'), xsig)
+            np.save(join(edir, s, f'{s}_{dnames[j]}_snr.npy'), xsnr)
+            np.save(join(edir, s, f'{s}_{dnames[j]}_euler.npy'), xeuler)
+            np.save(join(edir, s, f'{s}_{dnames[j]}_shannon.npy'), xshan)
+            np.save(join(edir, s, f'{s}_{dnames[j]}_psnr.npy'), xpsnr)
+            np.save(join(edir, s, f'{s}_{dnames[j]}_ssim.npy'), xssim)
+            np.save(join(edir, s, f'{s}_{dnames[j]}_mse.npy'), xmse)
+                        
+            # Save as local variables
+            globals()[f'{dnames[j]}_sigma'] = xsig
+            globals()[f'{dnames[j]}_snr'] = xsnr
+            globals()[f'{dnames[j]}_euler'] = xeuler
+            globals()[f'{dnames[j]}_shannon'] = xshan
+            globals()[f'{dnames[j]}_psnr'] = xpsnr
+            globals()[f'{dnames[j]}_ssim'] = xssim
+            globals()[f'{dnames[j]}_mse'] = xmse
+            
+            # Save image - so we can plot the comparisons
+            # TODO plot comparisons btw the images
+            globals()[f'{dnames[j]}_nii'] = img
+            
+            del xsig, xsnr, xeuler, xshan, xpsnr, xssim, xmse, img
 
         # load nosie sigma vals calcucalated during preprocessing.
         bvals = np.load(join(edir, s, f'{s}_bvals.npy'))
-        s_base = np.load(join(edir, s, f'{s}_sigma_base.npy'))
-        s_gauss = np.load(join(edir, s, f'{s}_sigma_ap_gauss.npy'))
-        s_lpca = np.load(join(edir, s, f'{s}_sigma_ap_lpca.npy'))
-        s_mppca = np.load(join(edir, s, f'{s}_sigma_ap_mppca.npy'))
-        s_nlm = np.load(join(edir, s, f'{s}_sigma_ap_nlm.npy'))
-
-        # Load images and perform further calculations
-        ibase, abase = load(join(edir, s, f'{s}_AP.nii'))
-        igauss, agauss = load(join(edir, s, f'{s}_ap_gauss.nii'))
-        ilpca, alpca = load(join(edir, s, f'{s}_ap_lpca.nii'))
-        imppca, amppca = load(join(edir, s, f'{s}_ap_mppca.nii'))
-        inlm, anlm = load(join(edir, s, f'{s}_ap_nlm.nii'))
-        ip2s, ap2s = load(join(edir, s ,f'{s}_AP_denoised.nii'))
-
-        # Calculate all single image metrics
-        def single_i_metrics(img):
-            # Calculate metrics
-            psnr_img = psnr(img, img, data_range=img.max() - img.min())
-            ssim_img = ssim(img, img, data_range=img.max() - img.min())
-            mse_img = mse(img, img)
-            euler_img = euler(img)
-            shannon_img = shannon(img)
-            snr_img = snr(img)
-
-            # Return metrics
-            return psnr_img, ssim_img, mse_img, euler_img, shannon_img, snr_img
-
-
+        
 
         # bvals are not precisely reported, so we need to interpolate them.
         bvals_corrected = []
@@ -97,15 +157,54 @@ if exists(edir):
 
         df = pd.DataFrame(data={'bvals': bvals,
                                 'bvals_corrected': bvals_corrected,
-                                's_base': s_base,
-                                's_gauss': s_gauss,
-                                's_lpca': s_lpca,
-                                's_mppca': s_mppca,
-                                's_nlm': s_nlm})
-        print(df['bvals_corrected'].value_counts())
+                                'base_sigma': base_sig,
+                                'base_snr': base_snr,
+                                'base_shannon': base_sha,
+                                'base_euler': base_sha,
+                                'gauss_sigma': gauss_sigma,
+                                'gauss_snr': gauss_snr,
+                                'gauss_shannon': gauss_shannon,
+                                'gauss_euler': gauss_euler,
+                                'gauss_psnr': gauss_psnr,
+                                'gauss_ssim': gauss_ssim,
+                                'gauss_mse': gauss_mse,
+                                'lpca_sigma': lpca_sigma,
+                                'lpca_snr': lpca_snr,
+                                'lpca_shannon': lpca_shannon,
+                                'lpca_euler': lpca_euler,
+                                'lpca_psnr': lpca_psnr,
+                                'lpca_ssim': lpca_ssim,
+                                'lpca_mse': lpca_mse,
+                                'mppca_sigma': mppca_sigma,
+                                'mppca_snr': mppca_snr,
+                                'mppca_shannon': mppca_shannon,
+                                'mppca_euler': mppca_euler,
+                                'mppca_psnr': mppca_psnr,
+                                'mppca_ssim': mppca_ssim,
+                                'mppca_mse': mppca_mse,
+                                'nlm_sigma': nlm_sigma,
+                                'nlm_snr': nlm_snr,
+                                'nlm_shannon': nlm_shannon,
+                                'nlm_euler': nlm_euler,
+                                'nlm_psnr': nlm_psnr,
+                                'nlm_ssim': nlm_ssim,
+                                'nlm_mse': nlm_mse,
+                                'mrtrix_sigma': mrtrix_sigma,
+                                'mrtrix_snr': mrtrix_snr,
+                                'mrtrix_shannon': mrtrix_shannon,
+                                'mrtrix_euler': mrtrix_euler,
+                                'mrtrix_psnr': mrtrix_psnr,
+                                'mrtrix_ssim': mrtrix_ssim,
+                                'mrtrix_mse': mrtrix_mse,
+                                'p2s_sigma': p2s_sigma,
+                                'p2s_snr': p2s_snr,
+                                'p2s_shannon': p2s_shannon,
+                                'p2s_euler': p2s_euler,
+                                'p2s_psnr': p2s_psnr,
+                                'p2s_ssim': p2s_ssim,
+                                'p2s_mse': p2s_mse})
+        
+        df.to_csv(join(edir, s, f'{s}_metrics.csv'), index=False)
 
-        for b in [0, 700, 1000, 1200, 1500, 2500]:
-            df[df['bvals_corrected'] == b].mean().to_csv(join('denoise_test', s, f'{s}_b{b}_means.csv'))
-            df[df['bvals_corrected'] == b].std().to_csv(join('denoise_test', s, f'{s}_b{b}_stdev.csv'))
 else:
     print('Unable to find subjects directory')
