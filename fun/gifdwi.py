3c8c19ce080ae7c8573ee199a3f16b0fdf946108
Aleksander Nitka <aleksander.nitka@uj.edu.pl>
2022-09-28 16:01:45 +0200

    fixed paths in gifdwi

--- fun/gifdwi.py
+++ fun/gifdwi.py
@@ -1,6 +1,6 @@
 import argparse
 import matplotlib.pyplot as plt
-from os.path import join, exists
+from os.path import join, exists, dirname
 from os import listdir as ls
 from os import remove
 from dipy.io.image import load_nifti
@@ -12,6 +12,9 @@ a.add_argument('output', help='Output: path to output GIF')
 a.add_argument('-s', '--slice', help='Slice to use for GIF', default=55, type=int)
 a.add_argument('-t', '--title', help='Title for GIF', default='DWI', type=str)
 a = a.parse_args()
+
+# Need path to dump tmp images
+tmpDir = dirname(a.output)
 
 try:
     img, aff = load_nifti(a.input)
@@ -34,18 +37,18 @@ for v in range(img.shape[-1]):
     ax.flat[1].imshow(img[:, a.slice, :, v].T, origin='lower', cmap='gray')
     ax.flat[2].imshow(img[a.slice, :, :, v].T, origin='lower', cmap='gray')
     
-    fig.savefig(join(a.output, f'tmp_img{1000+v}.png'))
+    fig.savefig(join(tmpDir, f'tmp_img{1000+v}.png'))
     plt.close(fig)
 
 # Make GIF
 print('Compiling GIF...')
-images = [Image.open(join(a.output, i)) for i in ls(join(a.output)) if i.endswith('.png') and i.startswith('tmp_img')]
+images = [Image.open(join(tmpDir, i)) for i in ls(join(tmpDir)) if i.endswith('.png') and i.startswith('tmp_img')]
 image1 = images[0]
 image1.save(join(a.output), format = "GIF", save_all=True, append_images=images[1:], duration=1000, loop=0)
 
 # Clean up
 print('Cleaning up...')
-for i in ls(join(a.output)):
+for i in ls(join(tmpDir)):
     if i.endswith('.png') and i.startswith('tmp_img'):
-        remove(join(a.output, i))
+        remove(join(tmpDir, i))
 
