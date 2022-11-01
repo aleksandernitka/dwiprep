0746c79531ea1a57fc16460c9805912f9f7a487d
Aleksander Nitka <aleksander.nitka@uj.edu.pl>
2022-11-01 10:31:11 +0100

    added run_batch_topup.py

---
+++ run_batch_topup.py
@@ -0,0 +1,15 @@
+import argparse
+from main import DwiPreprocessingClab
+
+args = argparse.ArgumentParser()
+args.add_argument('input', type=str, required=True, help='Input CSV file')
+args.add_argument('taks', type=str, required=True, help='Task name')
+args.add_argument('-w', '--wait', type=int, default=0, help='Wait time')
+args = argparse.parse_args()
+
+p = DwiPreprocessingClab(task=args.task, mode='l',\
+    input=args.input, \
+    datain='/mnt/nasips/COST_mri/derivatives/dwi/',\
+    dataout='/mnt/nasips/COST_mri/derivatives/dwi/')
+
+p.topup(skip_processed=True, wait=args.wait)
