e4c4d7a89908f64df8bb2c3be3d3fe1464cdccb7
aleksandernitka <aleksandernitka@uj.edu.pl>
2022-11-03 17:02:14 +0100

    topup experiment

--- experiments/test_topup/experiment_topup.py
+++ experiments/test_topup/experiment_topup.py
@@ -6,6 +6,7 @@ args.add_argument('data_dir', help='Data directory')
 args = args.parse_args()
 
 class topupExperiment():
+
     def __init__(self, sub, datadir):
 
         from dipy.io.image import load_nifti, save_nifti
@@ -15,12 +16,17 @@ class topupExperiment():
         from shutil import copyfile
         import subprocess as sp
         import json
+        import time as t
 
         self.sub = sub
         self.datadir = datadir
 
-        # mkdir in tmp
+        # mkdir in data
         mkdir(join('data', sub))
+
+        experiment = ['oneb0_raw', 'allb0_raw', 'oneb0_gibbs', 'allb0_gibbs', 'oneb0_mppca', 'allb0_mppca']
+        for e in experiment:
+            mkdir(join('data', sub, e))
 
         # Load the data
         files = [f'{self.sub}_AP.nii', f'{self.sub}_PA.nii',\
@@ -30,8 +36,8 @@ class topupExperiment():
             
         for file in files:
             try:
-                copyfile(join(datadir, sub, file), join('tmp', sub, file))
-                print(f'topup: Copied {file} to tmp folder')
+                copyfile(join(datadir, sub, file), join('data', sub, file))
+                print(f'topup: Copied {file} to data folder')
             except:
                 print(f'Could not copy file {file}')
                 continue
@@ -54,58 +60,88 @@ class topupExperiment():
         # allb0_mppca
 
         # Extract single b0 from all steps
-        self.sp.run(f'fslroi {join("tmp", self.sub, self.sub+"_AP.nii")} {join("tmp", self.sub, self.sub+"_AP_oneb0.nii.gz")} 0 1', shell=True)
-        self.sp.run(f'fslroi {join("tmp", self.sub, self.sub+"_PA.nii")} {join("tmp", self.sub, self.sub+"_PA_oneb0.nii.gz")} 0 1', shell=True)
-        self.sp.run(f'fslroi {join("tmp", self.sub, self.sub+"_AP_gib.nii.gz")} {join("tmp", self.sub, self.sub+"_AP_gib_oneb0.nii.gz")} 0 1', shell=True)
-        self.sp.run(f'fslroi {join("tmp", self.sub, self.sub+"_PA_gib.nii.gz")} {join("tmp", self.sub, self.sub+"_PA_gib_oneb0.nii.gz")} 0 1', shell=True)
-        self.sp.run(f'fslroi {join("tmp", self.sub, self.sub+"_AP_gib_mppca.nii.gz")} {join("tmp", self.sub, self.sub+"_AP_gib_mppca_oneb0.nii.gz")} 0 1', shell=True)
-        self.sp.run(f'fslroi {join("tmp", self.sub, self.sub+"_PA_gib_mppca.nii.gz")} {join("tmp", self.sub, self.sub+"_PA_gib_mppca_oneb0.nii.gz")} 0 1', shell=True)
+        # Input files
+        ap_raw = join("data", self.sub, self.sub+"_AP.nii")
+        pa_raw = join("data", self.sub, self.sub+"_PA.nii")
+        ap_gib = join("data", self.sub, self.sub+"_AP_gib.nii.gz")
+        pa_gib = join("data", self.sub, self.sub+"_PA_gib.nii.gz")
+        ap_mppca = join("data", self.sub, self.sub+"_AP_gib_mppca.nii.gz")
+        pa_mppca = join("data", self.sub, self.sub+"_PA_gib_mppca.nii.gz")
 
-        # Extract all b0s from all steps
-        # load gradient table
-        #try:
+        # Output files, extracted b0s for oneb0 condition
+        ap_raw_oneb0 = join("data", self.sub, 'oneb0_raw', self.sub+"_AP_raw_oneb0.nii.gz")
+        pa_raw_oneb0 = join("data", self.sub, 'oneb0_raw', self.sub+"_PA_raw_oneb0.nii.gz")
+        ap_gib_oneb0 = join("data", self.sub, 'oneb0_gibbs', self.sub+"_AP_gib_oneb0.nii.gz")
+        pa_gib_oneb0 = join("data", self.sub, 'oneb0_gibbs', self.sub+"_PA_gib_oneb0.nii.gz")
+        ap_mppca_oneb0 = join("data", self.sub, 'oneb0_mppca', self.sub+"_AP_mppca_oneb0.nii.gz")
+        pa_mppca_oneb0 = join("data", self.sub, 'oneb0_mppca', self.sub+"_PA_mppca_oneb0.nii.gz")
+
+        # Output files, extracted b0s for allb0 condition
+        ap_raw_allb0 = join("data", self.sub, 'allb0_raw', self.sub+"_AP_raw_allb0.nii.gz")
+        pa_raw_allb0 = join("data", self.sub, 'allb0_raw', self.sub+"_PA_raw_allb0.nii.gz")
+        ap_gib_allb0 = join("data", self.sub, 'allb0_gibbs', self.sub+"_AP_gib_allb0.nii.gz")
+        pa_gib_allb0 = join("data", self.sub, 'allb0_gibbs', self.sub+"_PA_gib_allb0.nii.gz")
+        ap_mppca_allb0 = join("data", self.sub, 'allb0_mppca', self.sub+"_AP_mppca_allb0.nii.gz")
+        pa_mppca_allb0 = join("data", self.sub, 'allb0_mppca', self.sub+"_PA_mppca_allb0.nii.gz")
+
+        # Merged AP-PA for topup
+        appa_raw_oneb0 = join("data", self.sub, 'oneb0_raw', self.sub+"_AP-PA_raw_oneb0.nii.gz")
+        appa_gib_oneb0 = join("data", self.sub, 'oneb0_gibbs', self.sub+"_AP-PA_gib_oneb0.nii.gz")
+        appa_mppca_oneb0 = join("data", self.sub, 'oneb0_mppca', self.sub+"_AP-PA_mppca_oneb0.nii.gz")
+        appa_raw_allb0 = join("data", self.sub, 'allb0_raw', self.sub+"_AP-PA_raw_allb0.nii.gz")
+        appa_gib_allb0 = join("data", self.sub, 'allb0_gibbs', self.sub+"_AP-PA_gib_allb0.nii.gz")
+        appa_mppca_allb0 = join("data", self.sub, 'allb0_mppca', self.sub+"_AP-PA_mppca_allb0.nii.gz")
+
+        # Extract the b0s SINGLE b0 in each direction
+        sp.run(f'fslroi {ap_raw} {ap_raw_oneb0} 0 1', shell=True)
+        sp.run(f'fslroi {pa_raw} {pa_raw_oneb0} 0 1', shell=True)
+        sp.run(f'fslroi {ap_gib} {ap_gib_oneb0} 0 1', shell=True)
+        sp.run(f'fslroi {pa_gib} {pa_gib_oneb0} 0 1', shell=True)
+        sp.run(f'fslroi {ap_mppca} {ap_mppca_oneb0} 0 1', shell=True)
+        sp.run(f'fslroi {pa_mppca} {pa_mppca_oneb0} 0 1', shell=True)
+
+        # Merge the b0s, SINGLE b0 in each direction
+        sp.run(f'fslmerge -t {appa_raw_oneb0} {ap_raw_oneb0} {pa_raw_oneb0}', shell=True)
+        sp.run(f'fslmerge -t {appa_gib_oneb0} {ap_gib_oneb0} {pa_gib_oneb0}', shell=True)
+        sp.run(f'fslmerge -t {appa_mppca_oneb0} {ap_mppca_oneb0} {pa_mppca_oneb0}', shell=True)
+
+        
+        # Extract all b0s ALL b0s in each direction
+
         # Create b0 mask
-        try:
-            gtab = gradient_table(f'tmp/{sub}/{sub}_AP.bval', f'tmp/{sub}/{sub}_AP.bvec')
-        except:
-            print(f'{sub} Could not create gradient table')
-            continue
+        gtab = gradient_table(f'data/{sub}/{sub}_AP.bval', f'data/{sub}/{sub}_AP.bvec')
 
-        # Prepare data for topup
-        # Extract b0s
-        ap_vols = ['_AP.nii', '_AP_gib.nii.gz'] # for third step, download from the server, as it has already been done
-        pa_vols = ['_PA.nii', '_PA_gib.nii.gz']
-
-        for v in range(0, len(ap_vols)):
+        all_vols = [[ap_raw, pa_raw], [ap_gib, pa_gib], [ap_mppca, pa_mppca]]
+        all_file = [[ap_raw_allb0, pa_raw_allb0], [ap_gib_allb0, pa_gib_allb0], [ap_mppca_allb0, pa_mppca_allb0]]
+        all_merg = [appa_raw_allb0, appa_gib_allb0, appa_mppca_allb0]
+        
+        for i, v in enumerate(all_vols):
         
             try:
                 # Load volumes
-                dwi_ap, affine_ap = load_nifti(join('tmp', self.sub, self.sub+ap_vols[v]))
-                dwi_pa, affine_pa = load_nifti(join('tmp', self.sub, self.sub+pa_vols[v]))
+                dwi_ap, affine_ap = load_nifti(all_vols[i][0])
+                dwi_pa, affine_pa = load_nifti(all_vols[i][1])
 
                 # Extract b0s
                 b0s_ap = dwi_ap[:,:,:,gtab.b0s_mask]
                 b0s_pa = dwi_pa[:,:,:,[True, True, True, True, False]]
 
-                ap_name = self.sub+ap_vols[v].split('.')[0]+'_allb0.nii.gz'
-                pa_name = self.sub+pa_vols[v].split('.')[0]+'_allb0.nii.gz'
-                fi_name = self.sub+ap_vols[v].replace('AP', 'APPA').split('.')[0]+'_allb0.nii.gz'
-
                 # Save volumes of b0s
-                save_nifti(join("tmp", self.sub, ap_name), b0s_ap, affine_ap)
-                save_nifti(join("tmp", self.sub, pa_name), b0s_pa, affine_pa)
+                save_nifti(all_file[i][0], b0s_ap, affine_ap)
+                save_nifti(all_file[i][1], b0s_pa, affine_pa)
 
                 # Merge into one AP-PA file
-                self.sp.run(f'fslmerge -t {join("tmp", self.sub, ap_name)} {join("tmp", self.sub, pa_name)} {join("tmp", self.sub, self.sub+fi_name)}', shell=True)
+                sp.run(f'fslmerge -t {all_merg[i]} {all_file[i][0]} {all_file[i][1]}', shell=True)
+                print(f'Merged {all_file[i][0]} and {all_file[i][1]} into {all_merg[i]}')
             except:
-                print(f'{sub} Could not extract b0s')
+                print(f'{self.sub} Could not extract b0s')
                 continue 
         
         # Create topup config file
         # Load sidecar jsons and read TRT
         ds = ['AP', 'PA']
         for d in ds:
-            with open(join('tmp', sub, sub+f'_{d}.json')) as f:
+            with open(join('data', self.sub, self.sub+f'_{d}.json')) as f:
                 data = json.load(f)
                 ro = data['TotalReadoutTime']
                 if d == 'AP':
@@ -116,13 +152,12 @@ class topupExperiment():
 
 
         # Create config file oneb0
-        with open(join('tmp', sub, sub+'_acqparam_oneb0.txt'), 'w') as f:
-            f.write(f'0 -1 0 {ap_ro}')
-            f.write(f'0 1 0 {pa_ro}')
+        with open(join('data', self.sub, self.sub+'_acqparam_oneb0.txt'), 'w') as f:
+            f.write(f'0 -1 0 {ap_ro}\n0 1 0 {pa_ro}')
             f.close()
         
         # Create config file allb0s
-        with open(join("tmp", self.sub, self.sub+'_acqparam_allb0.txt'), "w") as f:
+        with open(join("data", self.sub, self.sub+'_acqparam_allb0.txt'), "w") as f:
             # for each vol in AP and for each vol in PA
             for v in range(0, b0s_ap.shape[3]):
                 f.write(f"0 -1 0 {ap_ro}\n")
@@ -130,20 +165,39 @@ class topupExperiment():
                 f.write(f"0 1 0 {pa_ro}\n")
             f.close()
 
-    
-        experiment = ['oneb0_raw', 'allb0_raw', 'oneb0_gibbs', 'allb0_gibbs', 'oneb0_mppca', 'allb0_mppca']
-        
-        for e in experiment:
-            print(f'Running experiment {e} for {sub}')
-            mkdir(join('data', sub, e))
-            # Run topup
-            if 'oneb0' in e:
-                acqparam = join('tmp', sub, sub+'_acqparam_oneb0.txt')
-            else:
-                acqparam = join('tmp', sub, sub+'_acqparam_allb0.txt')
-            
-            
+        with open (join("data", self.sub, self.sub+'_timings.txt'), "w") as f:
+            f.write('sub\texp\ttime')
 
-            self.sp.run(f'topup --imain={b0im} --datain={acqparam} --config=b02b0.cnf \
-            --out={self.join("tmp", sub, f"{sub}_topup_results")} \
-            --iout={self.join("tmp", sub, f"{sub}_b0_corrected.nii.gz")} -v', shell=True)
+        for e in experiment:
+            print(f'Running experiment {e} for {self.sub}')
+            
+            set_b0s = e.split('_')[0]
+            set_stp = e.split('_')[1]
+
+            # Setup topup
+            acqparam = join('data', self.sub, self.sub+f'_acqparam_{set_b0s}.txt')
+            
+            # Set imain
+            mainimg = join('data', self.sub, e, self.sub+f'_AP-PA_{set_stp}_{set_b0s}.nii.gz')
+            
+            print(f"Running topup for {self.sub} with {self.sub+f'_AP-PA_{set_stp}_{set_b0s}.nii.gz'} for {e}")
+
+            # timer start
+            tic = t.perf_counter()
+
+            sp.run(f'topup --imain={mainimg} --datain={acqparam} --config=b02b0.cnf \
+            --out={join("data", sub, e, f"{sub}_{e}_tp_res_")} \
+            --iout={join("data", sub, e, f"{sub}_{e}_b0_cor.nii.gz")}', shell=True)
+
+            # timer stop
+            toc = t.perf_counter()
+            
+            with open (join("data", self.sub, self.sub+'_timings.txt'), "a") as f:
+                f.write(f'{self.sub}\t{e}\t{toc-tic}\n')
+
+            print(f"Finished topup {e} for {self.sub} in {toc - tic:0.4f} seconds")
+
+        print(f"{self.sub} finished all experiments")
+        f.close()
+
+exp = topupExperiment(sub = args.subject, datadir=args.data_dir)
